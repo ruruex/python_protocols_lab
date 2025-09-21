f865ad3b4e6bf2b25bbf5857fbaa5a25782e96e0
ruruex <kgbxzh2000xp@hotmail.com>
2025-09-21 17:10:25 +0800

    home work

--- 2025-09-18/snmp_v2_4_get_all.py
+++ 2025-09-18/snmp_v2_4_get_all.py
@@ -27,59 +27,55 @@ import asyncio
 
 # SNMP v2 获取信息汇总
 def snmpv2_get_all(ip_address, community):
+    
+   
     # 主机名
     sys_name = asyncio.run(snmpv2_get(ip_address, community, "1.3.6.1.2.1.1.5.0", port=161))[1]
 
-    # cpmCPUTotal5min
+    # cpmCPUTotal5min, int
     # 实体5分钟平均的CPU使用率 取值范围：0～100 缺省值：0
-    cpu_usage = int(asyncio.run(snmpv2_get(ip_address,
-                                           community,
-                                           "1.3.6.1.4.1.9.9.109.1.1.1.1.8.7",
-                                           port=161))[1])
+    cpu_usage = int(asyncio.run(snmpv2_get(ip_address, community, "1.3.6.1.4.1.9.9.109.1.1.1.1.8.7",port=161))[1])
+    
     # print(cpu_usage)
-    # cpmCPUMemoryUsed
-    mem_used = int(asyncio.run(snmpv2_get(ip_address,
-                                          community,
-                                          "1.3.6.1.4.1.9.9.109.1.1.1.1.12.7",
-                                          port=161))[1])
-    # cpmCPUMemoryFree
-    mem_free = int(asyncio.run(snmpv2_get(ip_address,
-                                          community,
-                                          "1.3.6.1.4.1.9.9.109.1.1.1.1.13.7",
-                                          port=161))[1])
+    # cpmCPUMemoryUsed, int
+    mem_used = int(asyncio.run(snmpv2_get(ip_address,community,"1.3.6.1.4.1.9.9.109.1.1.1.1.12.7",  port=161))[1])
+    # cpmCPUMemoryFree, int
+    mem_free = int(asyncio.run(snmpv2_get(ip_address, community, "1.3.6.1.4.1.9.9.109.1.1.1.1.13.7", port=161))[1])
 
-    mem_percent = round((mem_used / (mem_used + mem_free)*100), 4)
+    # memory utilization in percenage, float
+    mem_percent = float(round((mem_used / (mem_used + mem_free)*100), 4))
 
-    # print(mem_percent)
 
     # -----------------------------------------------------
-    # 接口名称
+    # 接口名称, list of str
     raw_name_list = asyncio.run(snmpv2_getbulk(ip_address, community, "1.3.6.1.2.1.2.2.1.2", port=161))
     if_name_list = [raw_if_name[1] for raw_if_name in raw_name_list]
-    # print(if_name_list)
+    #print(if_name_list)
 
-    # 接口速率
+    # 接口速率, list of int
     raw_speed_list = asyncio.run(snmpv2_getbulk(ip_address, community, "1.3.6.1.2.1.2.2.1.5", port=161))
     if_speed_list = [raw_speed[1] for raw_speed in raw_speed_list]
-    # print(if_speed_list)
+    #print(if_speed_list)
 
-    # 获取接口管理状态
+    # 获取接口管理状态, list of int, 1 is up, 0 is down
     raw_status_list = asyncio.run(snmpv2_getbulk(ip_address, community, "1.3.6.1.2.1.2.2.1.7", port=161))
     if_status_list = [raw_status[1] for raw_status in raw_status_list]
-    # print(if_status_list)
+    #print(if_status_list)
 
-    # 进接口字节数
+    # 入接口字节数, list of int
     raw_in_bytes_list = asyncio.run(snmpv2_getbulk(ip_address, community, "1.3.6.1.2.1.2.2.1.10", port=161))
     if_in_bytes_list = [raw_in_bytes[1] for raw_in_bytes in raw_in_bytes_list]
-    # print(if_in_bytes_list)
+    #print(if_in_bytes_list)
 
-    # 出接口字节数
+    # 出接口字节数, list of int
     raw_out_bytes_list = asyncio.run(snmpv2_getbulk(ip_address, community, "1.3.6.1.2.1.2.2.1.16", port=161))
     if_out_bytes_list = [raw_out_bytes[1] for raw_out_bytes in raw_out_bytes_list]
-    # print(if_out_bytes_list)
+    #print(if_out_bytes_list)
+
 
     interface_list = []
-    for name, speed, status, in_bytes, out_bytes in zip(if_name_list, if_speed_list, if_status_list, if_in_bytes_list, if_out_bytes_list):
+    for name, speed, status, in_bytes, out_bytes in zip(if_name_list, if_speed_list, if_status_list, if_in_bytes_list, if_out_bytes_list): # zip as concated list
+        #print(name,speed, status, in_bytes, out_bytes)
         if int(in_bytes) and int(out_bytes):
             interface_list.append({
                 'interface_name': name,
--- 2025-09-19/write_to_influxdb.py
+++ 2025-09-19/write_to_influxdb.py
@@ -3,7 +3,9 @@ import os
 from pathlib import Path
 from datetime import datetime
 
-from influxdb import InfluxDBClient  # for 1.x version
+
+from influxdb import InfluxDBClient
+from pytz import UTC  # for 1.x version
 
 # 获取当前脚本的绝对路径所在目录
 current_script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -17,3 +19,94 @@ target_dir = os.path.join(parent_dir, "2025-09-18")
 sys.path.append(target_dir)
 
 from snmp_v2_4_get_all import snmpv2_get_all
+
+# get the db password
+influxdb_pwd = os.getenv("influxdb_pwd")
+if influxdb_pwd is None:
+    # 可以选择记录日志、抛出异常或采取其他恢复措施
+    sys.exit("错误：未找到 'influxdb_pwd' 环境变量。请检查环境配置。")
+             
+
+# InfluxDB vars
+INFLUXDB_HOST = "localhost"    # The container is running locally
+INFLUXDB_PORT = 8086           
+INFLUXDB_USER = "qytdbuser"    
+INFLUXDB_PASSWORD = influxdb_pwd
+INFLUXDB_DATABASE = "qytdb"    
+
+# Create influxDB connection
+client = InfluxDBClient(
+    host=INFLUXDB_HOST,
+    port=INFLUXDB_PORT,
+    username=INFLUXDB_USER,
+    password=INFLUXDB_PASSWORD,
+    database=INFLUXDB_DATABASE
+)
+
+# Create the DB
+client.create_database(INFLUXDB_DATABASE)
+
+def get_info_writedb(ip_address, community):
+    '''
+    Get information from the routers through SNMPv2, write the DB
+    '''
+    router_all_snmp_dict = snmpv2_get_all(ip_address, community)
+    
+
+    # Prepare the cpu, memory utilization data
+    data_point = {
+        "measurement": "router_metrics",  # table name
+        "tags": {                         # tags, unchanged fields
+            "ip_address": ip_address,
+            "device_type": "IOS-XE"
+        },
+        "time": datetime.now(tz=UTC).strftime("%Y-%m-%dT%H:%M:%S.%fZ"), 
+        "fields": {
+            "cpu_usage": router_all_snmp_dict["cpu_usage"],               # int类型
+            "mem_usage_percent": router_all_snmp_dict["mem_usage_percent"], # float类型
+            "mem_usage": router_all_snmp_dict["mem_usage"],               # int类型
+            "mem_free": router_all_snmp_dict["mem_free"],                 # int类型
+            "device_ip": router_all_snmp_dict["device_ip"]                # str类型（设备IP字符串）
+        }                       
+    }
+    
+    # 写入数据到InfluxDB
+    client.write_points([data_point])
+    print(f'data from {data_point["tags"]["ip_address"]} is written to the measurement: {data_point["measurement"]} ')
+
+    # Write inrefaces data to the DB
+    #if_bytes_body = []  # 用于存储多个接口的字典
+
+    for if_info in router_all_snmp_dict.get('interface_list',[]):
+        if if_info.get('in_bytes') and if_info.get('out_bytes'):
+            if_info_dict = {
+                            "measurement": "if_monitor",   # table name
+                            "time": datetime.now(tz=UTC).strftime("%Y-%m-%dT%H:%M:%S.%fZ"),  # timestamp, key of the time series DB
+                            "tags": {                      # tags for filter
+                                "device_ip": router_all_snmp_dict.get('device_ip'),
+                                "device_type": "IOS-XE",
+                                "interface_name": if_info.get('interface_name')
+                            },
+                            "fields": {                    # 字段，用于存储数据
+                                "in_bytes": if_info.get('in_bytes'),
+                                "out_bytes": if_info.get('out_bytes'),
+                            },
+                        }
+            
+        # 写入数据到InfluxDB
+        client.write_points([if_info_dict])
+
+    print(f'data from {if_info_dict["tags"]["device_ip"]} is written to the measurement: {if_info_dict["measurement"]} ')
+
+
+
+
+if __name__ == '__main__':
+    ip_address_list = ['10.128.1.51', '10.128.1.91']
+    community = 'tcpipro'
+
+    for ip in ip_address_list:
+        get_info_writedb(ip, community)
+    
+    # 关闭连接
+    client.close()
